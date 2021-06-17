4b448ef140bebbbbfe4980ab716c0543552100b0
Ali Akan-Narcis <ali.akan.narcis01@gmail.com>
2021-06-17 14:06:55 +0300

    custom exception class

--- ACO/MyException.h
+++ ACO/MyException.h
@@ -1,1 +1,17 @@
-#pragma once
+#pragma
+#include <iostream>
+#include <exception>
+
+using namespace std;
+
+class MyException {
+private:
+	string message;
+public:
+	MyException(string msg) : message(msg) {}
+
+	string toString()
+	{
+		return message;
+	}
+};
